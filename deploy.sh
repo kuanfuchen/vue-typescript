5a801e6649e5b19f29713edc13cc8e0387c1ae7e
kuanfuchen <creazy_fu@hotmail.com>
2024-02-04 19:32:45 +0800

    modify deploy

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,11 @@ npm run build
 cd dist
 
 # place .nojekyll to bypass Jekyll processing
-echo > .nojekyll
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 git init
-git checkout -B main
 git add -A
 git commit -m 'deploy'
 
