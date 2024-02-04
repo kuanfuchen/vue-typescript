a68cd085c7dc6d3c296d613ddaf0a5f36949282c
kuanfuchen <creazy_fu@hotmail.com>
2024-02-04 19:29:21 +0800

    setting vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base:'/',
+  base: process.env.NODE_ENV === 'production' ? '/vue-typescript/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
